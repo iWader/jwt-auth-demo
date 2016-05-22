1b902fcef25b250e16f6403aa4f8b4192bb00c47
Wade Urry <wade@iwader.co.uk>
2016-05-22 13:40:56 +0100

    Setup auth provider and guard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,10 +31,9 @@ class AuthServiceProvider extends ServiceProvider
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
-        Auth::viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
-            }
+        $this->app['auth']->viaRequest('api', function ($request)
+        {
+            return \App\User::where('email', $request->input('email'))->first();
         });
     }
 }
