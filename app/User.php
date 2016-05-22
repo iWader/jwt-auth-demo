f6846552ce7468dc866f0f72fe4a112a8e56125b
Wade Urry <wade@iwader.co.uk>
2016-05-22 13:41:29 +0100

    Implement JWTSubject on our User model

--- app/User.php
+++ app/User.php
@@ -7,10 +7,9 @@ use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements
-    AuthenticatableContract,
-    AuthorizableContract
+class User extends Model implements JWTSubject, AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable;
 
@@ -31,4 +30,14 @@ class User extends Model implements
     protected $hidden = [
         'password',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
